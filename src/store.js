d83c4cae9373c1980a7bc6cfe6928930af80db2f
Wagner Araújo <wagneraraujo15@gmail.com>
2021-05-03 19:55:35 -0300

    payload, actions e mutations

--- src/store.js
+++ src/store.js
@@ -6,5 +6,16 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     user: "Wagner"
+  },
+
+  //mudar inforacao do state
+  mutations: {
+    mudarUser(state) {
+      state.user = "Andri";
+    }
   }
+
+  //payload => enviar alguma ifnromacao junto com o commit => enviar como objeto para passar mais informacoes
+  //
+  //actions => sempre vai chamar uma mutacao para modificar um estado
 });
